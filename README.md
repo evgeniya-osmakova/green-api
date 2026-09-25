16a5a708b2edd0417bf3ae697738f3f4559b955c
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2026-09-25 12:52:40 +0200

    chore: initialize React project

---
+++ README.md
@@ -0,0 +1,5 @@
+# GREEN-API Telegram Chat
+
+Клиент Telegram на React, TypeScript и GREEN-API.
+
+Полная документация проекта будет добавлена на финальном этапе разработки.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <main className={styles.app}>
+      <h1 className={styles.app__title}>GREEN-API Chat</h1>
+    </main>
+  )
+}
+
+export default App
